1851eaf68638c2982bb64df0753c62de5ee209c9
Christian <christain@afrilish.com>
2023-09-21 23:43:53 +0100

    last min update

--- src/Firebase.tsx
+++ src/Firebase.tsx
@@ -1,28 +1,28 @@
-// src/firebase.ts
-import firebase from 'firebase/app';
-import 'firebase/auth';
+// // src/firebase.ts
+// import firebase from 'firebase/app';
+// import 'firebase/auth';
 
-const firebaseConfig = {
-  apiKey: import.meta.env.VITE_GALLERY_API_KEY,
-  authDomain: import.meta.env.VITE_GALLERY_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_GALLERY_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_GALLERY_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_GALLERY_MESSAGE_SENDER_ID,
-  appId: import.meta.env.VITE_GALLERY_APP_ID,
-};
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_GALLERY_API_KEY,
+//   authDomain: import.meta.env.VITE_GALLERY_AUTH_DOMAIN,
+//   projectId: import.meta.env.VITE_GALLERY_PROJECT_ID,
+//   storageBucket: import.meta.env.VITE_GALLERY_STORAGE_BUCKET,
+//   messagingSenderId: import.meta.env.VITE_GALLERY_MESSAGE_SENDER_ID,
+//   appId: import.meta.env.VITE_GALLERY_APP_ID,
+// };
 
-// Initialize Firebase
-const app = firebase.initializeApp(firebaseConfig);
+// // Initialize Firebase
+// const app = firebase.initializeApp(firebaseConfig);
 
-// Access the authentication module
-export const auth = firebase.auth();
+// // Access the authentication module
+// export const auth = firebase.auth();
 
-// Create a login function
-export const login = async (email: string, password: string) => {
-  try {
-    const userCredential = await auth.signInWithEmailAndPassword(email, password);
-    return userCredential.user;
-  } catch (error) {
-    throw error;
-  }
-};
+// // Create a login function
+// export const login = async (email: string, password: string) => {
+//   try {
+//     const userCredential = await auth.signInWithEmailAndPassword(email, password);
+//     return userCredential.user;
+//   } catch (error) {
+//     throw error;
+//   }
+// };
